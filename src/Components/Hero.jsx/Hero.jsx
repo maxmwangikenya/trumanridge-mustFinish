2b68fa4c4052068ecdb8e57031315ad590a69158
maxmwangikenya <maxmwangikenya@gmail.com>
2025-02-19 15:00:18 +0300

    header

--- src/Components/Hero.jsx/Hero.jsx
+++ src/Components/Hero.jsx/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import '../Hero.css';
+import Header from '../Header/Header';
 
 const Hero = () => {
   return (
     <div className='hero'>
-        <div className='left-h'>left side</div>
+        <div className='left-h'>
+           <Header/> 
+           <div className="the-best-ad">
+            <div></div>
+            <span>the best adult care center in town </span>
+           </div>
+        </div>
         <div className='right-h'>right side</div>
     </div>
   )
