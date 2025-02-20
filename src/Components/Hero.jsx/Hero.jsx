3254e8320b62d74aeb00aa00834877023ca7d458
maxmwangikenya <maxmwangikenya@gmail.com>
2025-02-20 16:23:19 +0300

    working on the nav bar

--- src/Components/Hero.jsx/Hero.jsx
+++ src/Components/Hero.jsx/Hero.jsx
@@ -20,8 +20,25 @@ const Hero = () => {
   <div>
     <span> Care Ever</span>
   </div>
+  <div>
+  <span>At TrumanRidge Adult Care Center, we provide compassionate support, ensuring comfort, dignity, and a fulfilling life for every individual in our care.</span>
 </div>
-
+</div>
+{/* figure */}
+<div className="figure">
+  <div>
+    <span>+40</span>
+    <span>QULIFIED EMPLOYEES</span>
+    </div>
+  <div>
+    <span>+200</span>
+    <span>MEMBERS JOINED</span>
+    </div>
+  <div>
+    <span>+200</span>
+    <span>SUCCESS STORIES</span>
+    </div>
+</div>
         </div>
         <div className='right-h'>right side</div>
     </div>
