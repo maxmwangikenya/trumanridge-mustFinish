e362f89ce2f20eab188e82e7cb573ad0d9fbd8e1
maxmwangikenya <maxmwangikenya@gmail.com>
2025-02-20 13:58:28 +0300

    added an ad

--- src/Components/Hero.jsx/Hero.jsx
+++ src/Components/Hero.jsx/Hero.jsx
@@ -6,11 +6,22 @@ const Hero = () => {
   return (
     <div className='hero'>
         <div className='left-h'>
-           <Header/> 
+           <Header/>
+  {/* the best ad */}
            <div className="the-best-ad">
             <div></div>
             <span>the best adult care center in town </span>
            </div>
+  {/* hero heading          */}
+  <div className="hero-text">
+  <div>
+    <span className="stroke-text">Get</span> Best
+  </div>
+  <div>
+    <span> Care Ever</span>
+  </div>
+</div>
+
         </div>
         <div className='right-h'>right side</div>
     </div>
